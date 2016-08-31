41f0ba90b8091cbd4f44e66e427a138f918888db
kmicic77 <kmicic77@gmail.com>
2016-08-31 01:05:03 +0100

    my project

---
+++ coinindex.py
@@ -0,0 +1,16 @@
+""" Choose your portfolio of cryptocurrencies """
+""" Program calculates how many units you should buy """
+""" to have coins proportional to their market cap """
+""" which is similar to Index investing """
+
+import os
+
+def display_menu():
+	print("\n\n\n   1. Display top cryptocurrencies")
+	print("   2. Choose your portfolio")
+	print("   3. Exit")
+
+os.system('cls')
+display_menu()
+
+
