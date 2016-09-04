c508eece6fd3bef137af71272d40f222a7bb4013
kmicic77 <kmicic77@gmail.com>
2016-09-04 22:04:22 +0100

    Creating spreadsheet almost ready
    Filling names and formulas

--- coinindex.py
+++ coinindex.py
@@ -5,6 +5,7 @@
 
 import os, json, urllib.request
 from openpyxl import Workbook
+from openpyxl.utils import coordinate_from_string
 
 
 def display_menu():
@@ -13,9 +14,9 @@ def display_menu():
 	print("   3. Exit")
 def display_top(data):
 	for i in range(30):
-		print ("{:>5}. {:>23}  {:>12} USD  Market Cap: {:>15,} USD".format(data[i]['rank'],data[i]['name'],
-		data[i]['price_usd'],data[i]['market_cap_usd']))
-	
+		
+		print ("{:>5}. {:>23}  {:>12} USD  Market Cap: {:>15,} USD".format(data[i]['rank'],data[i]['name'],data[i]['price_usd'],float(data[i]['market_cap_usd'])))
+
 	
 def choose_portfolio(data):
 	while True:
@@ -33,19 +34,19 @@ def choose_portfolio(data):
 		
 		for item in new_list:
 			print ("{:>5}. {:>23}  {:>12} USD  Market Cap: {:>15,} USD".format(item['rank'],
-			item['name'],item['price_usd'],item['market_cap_usd']))
+			item['name'],item['price_usd'],float(item['market_cap_usd'])))
 
 		money=float(input("\nHow much money would you like to invest (in USD) ? "))
 		total_market_cap=0
 
 		for item in new_list:
-			total_market_cap+=item['market_cap_usd']
+			total_market_cap+=float(item['market_cap_usd'])
 		
 		for item in new_list:
-			item['proportion']=item['market_cap_usd']/total_market_cap
+			item['proportion']=float(item['market_cap_usd'])/total_market_cap
 			item['worth_in_usd']=item['proportion']*money
-			item['units']=item['worth_in_usd']/item['price_usd']
-			item['in_btc']=item['worth_in_usd']/data[0]['price_usd'] #bitcoin price in USD
+			item['units']=item['worth_in_usd']/float(item['price_usd'])
+			item['in_btc']=item['worth_in_usd']/float(data[0]['price_usd']) #bitcoin price in USD
 			
 		print ("\nTo have your investment proportional to crypto market cap your portfolio should look like this: \n")
 		
@@ -91,6 +92,47 @@ def create_xls(data):
 	ws['I4']="How much I have now"
 	ws['J4']="How much to buy"
 	ws['K4']="Balance"
+
+	ws.column_dimensions['C'].width = 15
+	ws.column_dimensions['D'].width = 20
+	ws.column_dimensions['E'].width = 12
+	ws.column_dimensions['F'].width = 20
+	ws.column_dimensions['G'].width = 20
+	ws.column_dimensions['H'].width = 12
+	ws.column_dimensions['I'].width = 20
+	ws.column_dimensions['J'].width = 20
+	ws.column_dimensions['K'].width = 12
+
+	# filling worsheet with names and values
+	#formula_1 calculates how much to buy (target minus how much I have now)
+	#formula_2 calculates balance - how many percent actual holdings differ from target
+	
+	for i in range(len(data)):
+		y=str(ws.cell(row=i+5,column=8).column)
+		x=str(ws.cell(row=i+5,column=8).row)
+		h5=y+x
+		y1=str(ws.cell(row=i+5,column=9).column)
+		x1=str(ws.cell(row=i+5,column=9).row)
+		i5=y1+x1
+		formula_1='='+h5+'-'+i5
+		formula_2='='+h5+"/"+i5+"-1"
+
+
+		ws.cell(row=(i+5),column=3,value=data[i]['name'])
+		ws.cell(row=(i+5),column=4,value=float(data[i]['market_cap_usd']))
+		ws.cell(row=(i+5),column=5,value=data[i]['proportion'])
+		ws.cell(row=(i+5),column=6,value=data[i]['worth_in_usd'])
+		ws.cell(row=(i+5),column=7,value=float(data[i]['price_usd']))
+		ws.cell(row=(i+5),column=8,value=data[i]['units'])
+		ws.cell(row=(i+5),column=9,value=0)
+		ws.cell(row=(i+5),column=10,value=formula_1)
+		ws.cell(row=(i+5),column=11,value=formula_2)
+
+	#formula_3 calculates total market cap
+
+	formula_3='=SUM(D5:D'+str(len(data)+4)
+	ws.cell(row=len(data)+7,column=3,value='Total:')
+	ws.cell(row=len(data)+7,column=4,value=formula_3)
 
 	file_name=input('Save portfolio as...(program will use Excel extension .xlsx) : ')
 	file_name+=".xlsx"
